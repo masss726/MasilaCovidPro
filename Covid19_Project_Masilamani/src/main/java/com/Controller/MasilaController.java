6e4ff85374434b55f970f5c90db5e16ebeff6e42
masss726 <nmasilamani615@gmail.com>
2025-03-18 16:35:10 +0530

    conformation tymleaf test1 :pass

--- Covid19_Project_Masilamani/src/main/java/com/Controller/MasilaController.java
+++ Covid19_Project_Masilamani/src/main/java/com/Controller/MasilaController.java
@@ -1,6 +1,6 @@
 package com.Controller;
 import java.util.Map;
-
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +12,9 @@ public class MasilaController {
 
     //This method is used to get the world data
     @GetMapping("/world")
-    public String getWorldData() {
+    public String getWorldData(Model m) {
         Map<String,Object> worldDatas=covidDatas.getCovidWorld();
+        m.addAttribute("worldDatas","Hi it is working");
         return "indexHome";
     }
 
